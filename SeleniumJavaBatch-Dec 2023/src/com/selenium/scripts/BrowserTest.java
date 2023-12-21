a903fce26f5adb54a900d5a145d0f5437fe81c95
nareshbabumandula <naresh223@gmail.com>
2023-12-21 08:27:03 +0530

    Initial Commit

---
+++ SeleniumJavaBatch-Dec 2023/src/com/selenium/scripts/BrowserTest.java
@@ -0,0 +1,21 @@
+package com.selenium.scripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserTest {
+
+	static WebDriver driver;
+	
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./browsers/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.mycontactform.com");
+		driver.manage().window().maximize();
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+}
