c32a5cc8e3d2f128d3e58f97b9b2d55006da0439
nareshbabumandula <naresh223@gmail.com>
2023-12-22 09:03:45 +0530

    22-12-2023:Naresh: Working with multiple browsers in Selenium

--- SeleniumJavaBatch-Dec 2023/src/com/selenium/scripts/BrowserTest.java
+++ SeleniumJavaBatch-Dec 2023/src/com/selenium/scripts/BrowserTest.java
@@ -2,14 +2,31 @@ package com.selenium.scripts;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserTest {
 
 	static WebDriver driver;
-	
-	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "./browsers/chromedriver.exe");
-		driver = new ChromeDriver();
+
+	public static void launchBrowser(String browser) throws InterruptedException {
+		switch (browser.toLowerCase().trim()) {
+		case "chrome":
+			System.setProperty("webdriver.chrome.driver", "./browsers/chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			System.setProperty("webdriver.edge.driver", "./browsers/msedgedriver.exe");
+			driver = new EdgeDriver();
+			break;
+		case "firefox":
+			System.setProperty("webdriver.gecko.driver", "./browsers/geckodriver.exe");
+			driver = new FirefoxDriver();
+			break;
+		default:
+			System.out.println("Invalid browser..!");
+			break;
+		}
 		driver.get("https://www.mycontactform.com");
 		driver.manage().window().maximize();
 		System.out.println(driver.getCurrentUrl());
@@ -17,6 +34,11 @@ public class BrowserTest {
 		driver.navigate().to("https://www.mycontactform.com/samples.php");
 		Thread.sleep(3000);
 		driver.quit();
+
 	}
 
+	public static void main(String[] args) throws InterruptedException {
+		launchBrowser("EDGE");
+		launchBrowser("Chrome ");
+	}
 }
